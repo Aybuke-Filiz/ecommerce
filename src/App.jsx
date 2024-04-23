acdcd456f3ba6c341763e760fa98466653b027a8
Aybuke-Filiz <aybukeyavuz123@gmail.com>
2024-04-23 09:38:05 +0300

    T1: Project Setup Fixes #2

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div className='bg-green'>
+      <div className='bg-black'>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
