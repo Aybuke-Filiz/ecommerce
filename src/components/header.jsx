540ef1a302dec2c217f629a6ef29e63801cee684
Aybuke-Filiz <aybukeyavuz123@gmail.com>
2024-04-24 12:51:28 +0300

    başlangıç

---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+import Telefon from "../assets/Header/aramaicon.png"
+export default function Header(){
+    return(
+        <>
+        <container>
+            <div className="navBarDark">
+                <div>
+                    {Telefon} (225)555-0118
+                </div>
+
+            </div>
+        </container>
+        </>
+    )
+}
